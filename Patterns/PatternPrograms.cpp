84341f4e83639c624688bdbd056df68f625614e6
Animesh Das <animesdas.2013@gmail.com>
2023-01-21 02:21:28 +0530

    modified pattern file

--- Patterns/PatternPrograms.cpp
+++ Patterns/PatternPrograms.cpp
@@ -149,8 +149,93 @@ void pattern10(int n){
 	}
 }
 
+void pattern11(int n){
+	int start = 1;
+	for(int i = 0; i<n; i++){
+		if(i%2==0){
+			start = 1;
+		}
+		else{
+			start = 0;
+		}
+		for(int j = 0; j<=i; j++){
+			cout << start;
+			start = 1 - start;
+		}
+		cout << endl;
+	}
+}
+
+void pattern12(int n){
+	int space = 2*(n-1);
+	for(int i = 1; i<=n; i++){
+		
+		for(int j = 1; j<=i; j++){
+			cout << j;
+		}
+		
+		for(int j = 1; j<=space; j++){
+			cout << " ";
+		}
+		
+		for(int j = i; j>=1; j--){
+			cout << j;
+		}
+		
+		cout << endl;
+		space -= 2;
+		
+	}
+}
+
+void pattern13(int n){
+	int start = 1;
+	for(int i = 1; i<=n; i++){
+		for(int j = 1; j<=i; j++){
+			cout << start << " ";
+			start = start + 1;
+		}
+		cout<<endl;
+		
+	}
+}
+
+void pattern14(int n){
+	for(int i = 0; i<n; i++){
+		for(char j = 'A'; j<='A'+i; j++){
+			cout << j << " ";
+		}
+		cout<<endl;
+		
+	}
+}
+
+void pattern15(int n){
+	for(int i = 0; i<n; i++){
+		for(char c = 'A'; c <= 'A' + (n-i-1); c++){
+			cout << c << " ";
+		}
+		
+		cout << endl;
+	}
+}
+
 int main(){
 	int n;
 	cin>>n;
-	pattern10(n);
+//	pattern1(n);
+//	pattern2(n);
+//	pattern3(n);
+//	pattern4(n);
+//	pattern5(n);
+//	pattern6(n);
+//	pattern7(n);
+//	pattern8(n);
+//	pattern9(n);
+//	pattern10(n);
+//	pattern11(n);
+//	pattern12(n);
+//	pattern13(n);
+//	pattern14(n);	
+	pattern15(n);
 }
