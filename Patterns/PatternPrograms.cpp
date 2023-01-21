e3bf7252752a1bc0e781c91c1cd05adaebf33803
Animesh Das <animesdas.2013@gmail.com>
2023-01-22 00:55:54 +0530

    more patterns added

--- Patterns/PatternPrograms.cpp
+++ Patterns/PatternPrograms.cpp
@@ -220,22 +220,176 @@ void pattern15(int n){
 	}
 }
 
+void pattern16(int n){
+	for(int i = 0; i<n; i++){
+		char ch = 'A' + i;
+		for(int j = 0; j<=i; j++){
+			cout << ch;
+		}
+		
+		cout << endl;
+	}
+}
+
+void pattern17(int n){
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<n-i-1; j++){
+			cout<<"  ";
+		}
+		
+		char ch = 'A';
+		int breakpoint = (2*i+1)/2;
+		
+		for(int j = 1; j<=2*i+1; j++){
+			cout<<ch << " ";
+			if(j<=breakpoint){
+				ch++;
+			}
+			else{
+				ch--;
+			}
+			
+		}	
+		
+		for(int j = 0; j<n-i+1; j++){
+			cout<<"  ";
+		}
+			
+		cout<<endl;
+	}
+}
+
+void pattern18(int n){
+	for(int i = 0; i<n; i++){
+		for(char ch = 'E'- i; ch<='E'; ch++){
+			cout << ch;
+		}
+		cout << endl;
+	}
+}
+
+void pattern19(int n){
+	
+	int iniS = 0;
+	for(int i = 0; i<n; i++){
+		
+		for(int j = 0; j<n-i; j++){
+			cout << "*";	
+		}
+	
+		for(int j = 0; j<iniS; j++){
+			cout << " ";
+		}
+	
+		for(int j = 0; j<n-i; j++){
+			cout << "*";
+		}
+		
+		iniS += 2;
+		cout << endl;
+		
+	}
+	
+	iniS = 2*n-2;
+	for(int i = 1; i<=n; i++){
+		
+		for(int j = 1; j<=i; j++){
+			cout << "*";	
+		}
+	
+		for(int j = 0; j<iniS; j++){
+			cout << " ";
+		}
+	
+		for(int j = 1; j<=i; j++){
+			cout << "*";
+		}
+		
+		iniS -= 2;
+		cout << endl;
+		
+	}
+		
+}
+
+void pattern20(int n){
+	
+	int spaces = 2*n-2;
+	
+	for(int i = 1; i<=2*n-1; i++){
+		int stars = i;
+		if(i > n) stars = 2*n-i;
+		
+		for(int j = 1; j<=stars; j++){
+			cout << "*";
+		}
+		
+		for(int j = 1; j<=spaces; j++){
+			cout << " ";
+		}
+		
+		for(int j = 1; j<=stars; j++){
+			cout << "*";
+		}
+		
+		cout << endl;
+		if(i<n) spaces-=2;
+		else spaces+=2;
+				
+	}
+}
+
+void pattern21(int n){
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<n; j++){
+			if(i==0 || j==0 || i==n-1 || j==n-1){
+				cout << "*";
+			}
+			else{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void pattern22(int n){
+	for(int i = 0; i<2*n-1; i++){
+		for(int j = 0; j<2*n-1; j++){
+			int top = i;
+			int left = j;
+			int right = (2*n - 2) - j;
+			int down = (2*n - 2) - i;
+			cout << (n - min(min(top,down), min(left,right)));
+		}
+		
+		cout << endl;
+	}
+}
+
 int main(){
 	int n;
 	cin>>n;
-//	pattern1(n);
-//	pattern2(n);
-//	pattern3(n);
-//	pattern4(n);
-//	pattern5(n);
-//	pattern6(n);
-//	pattern7(n);
-//	pattern8(n);
-//	pattern9(n);
-//	pattern10(n);
-//	pattern11(n);
-//	pattern12(n);
-//	pattern13(n);
-//	pattern14(n);	
+	pattern1(n);
+	pattern2(n);
+	pattern3(n);
+	pattern4(n);
+	pattern5(n);
+	pattern6(n);
+	pattern7(n);
+	pattern8(n);
+	pattern9(n);
+	pattern10(n);
+	pattern11(n);
+	pattern12(n);
+	pattern13(n);
+	pattern14(n);	
 	pattern15(n);
+	pattern16(n);
+	pattern17(n);
+	pattern18(n);
+	pattern19(n);
+	pattern20(n);
+	pattern21(n);
+	pattern22(n);
 }
